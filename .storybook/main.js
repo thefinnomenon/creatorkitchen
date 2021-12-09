72396d80dfae9d1d22abc98444622f00b70bbcbb
Chris Finn <chris@finnternet.com>
2021-12-09 16:08:31 -0500

    chore: add Storybook

---
+++ .storybook/main.js
@@ -0,0 +1,25 @@
+const path = require('path')
+
+module.exports = {
+  "stories": [
+    "../stories/**/*.stories.mdx",
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    {
+      name: "@storybook/addon-postcss",
+      options: {
+        postcssLoaderOptions: {
+          implementation: require("postcss"),
+        },
+      },
+    },
+  ],
+  "framework": "@storybook/react",
+  "core": {
+    "builder": "webpack5"
+  },
+  "typescript": { "reactDocgen": false },
+}
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Tiptap from '../components/Tiptap'
+import Tiptap from '../components/TipTap'
 
 export default function Home() {
   return (
