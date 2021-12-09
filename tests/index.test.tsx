1a3b941ddd50c60f67ec3ee5a4f81f66d3e1cc22
Chris Finn <chris@finnternet.com>
2021-12-09 16:18:52 -0500

    chore: setup jest

---
+++ tests/index.test.tsx
@@ -0,0 +1,13 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Home from '../pages/index';
+
+describe('Home', () => {
+  it('renders', () => {
+    render(<Home />);
+  });
+});
