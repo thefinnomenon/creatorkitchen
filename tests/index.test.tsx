480657aa737dbba9c98ba1110e9099798728f670
Chris Finn <chris@finnternet.com>
2022-02-22 18:51:48 -0500

    feat: autosave. posts panel

--- tests/index.test.tsx
+++ tests/index.test.tsx
@@ -7,7 +7,7 @@ import { render, screen } from '@testing-library/react';
 import Home from '../pages/index';
 
 describe('Home', () => {
-  // it('renders', () => {
-  //   render(<Home />);
-  // });
+  it('renders', () => {
+    render(<Home />);
+  });
 });
